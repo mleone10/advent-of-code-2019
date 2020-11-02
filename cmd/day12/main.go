e462bf3d9017fa8d4a147dc0d08f6c24e90de09d
Mario Leone <mleone10@gmail.com>
2020-11-02 12:12:08 -0500

    Day 12: Solve part 1

--- cmd/day12/main.go
+++ cmd/day12/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	aoc "github.com/mleone10/advent-of-code-2019"
+)
 
 const n = 1000
 
@@ -33,22 +37,29 @@ func main() {
 	log.Printf("Total energy after %d steps: %d", n, s.getEnergy())
 }
 
-func (s system) step() {
+func (s *system) step() {
 	s.applyGravity()
 	s.applyVelocity()
 }
 
-func (s system) applyGravity() {
-
-}
-
-func (s system) applyVelocity() {
-	for _, m := range s.ms {
-		m.applyVelocity()
+func (s *system) applyGravity() {
+	for i := range s.ms {
+		for j, n := range s.ms {
+			if i == j {
+				continue
+			}
+			s.ms[i].applyGravity(n)
+		}
 	}
 }
 
-func (s system) getEnergy() int {
+func (s *system) applyVelocity() {
+	for i := range s.ms {
+		s.ms[i].applyVelocity()
+	}
+}
+
+func (s *system) getEnergy() int {
 	var sum int
 	for _, m := range s.ms {
 		sum += m.getEnergy()
@@ -56,28 +67,47 @@ func (s system) getEnergy() int {
 	return sum
 }
 
-func (m moon) applyVelocity() {
+func (m *moon) applyGravity(n moon) {
+	m.vel.applyGravity(m.pos, n.pos)
+}
+
+func (m *moon) applyVelocity() {
 	m.pos.add(m.vel)
 }
 
-func (m moon) getEnergy() int {
+func (m *moon) getEnergy() int {
 	return m.getPotentialEnergy() * m.getKineticEnergy()
 }
 
-func (m moon) getPotentialEnergy() int {
+func (m *moon) getPotentialEnergy() int {
 	return m.pos.sum()
 }
 
-func (m moon) getKineticEnergy() int {
+func (m *moon) getKineticEnergy() int {
 	return m.vel.sum()
 }
 
-func (v vector) add(r vector) {
+func (v *vector) applyGravity(m, n vector) {
+	gravityDelta := func(a, b int) int {
+		if a > b {
+			return -1
+		}
+		if a == b {
+			return 0
+		}
+		return 1
+	}
+	v.x += gravityDelta(m.x, n.x)
+	v.y += gravityDelta(m.y, n.y)
+	v.z += gravityDelta(m.z, n.z)
+}
+
+func (v *vector) add(r vector) {
 	v.x += r.x
 	v.y += r.y
 	v.z += r.z
 }
 
-func (v vector) sum() int {
-	return v.x + v.y + v.z
+func (v *vector) sum() int {
+	return aoc.Abs(v.x) + aoc.Abs(v.y) + aoc.Abs(v.z)
 }
